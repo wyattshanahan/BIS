4b8c8047849108102fbc9202dc9c86306629e99c
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-02-05 14:34:21 -0600

    Add template files

---
+++ template/results.php
@@ -0,0 +1,24 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>Results</title>
+  <meta name="viewport" content="width=device-width"> <!--make page adjust to device screen size-->
+  <meta name="author" content="Wyatt Shanahan" />
+  <link rel="stylesheet" href="style.css"> 
+  <link rel="icon" type="image/x-icon" href="vibin.gif"> <!-- set icon-->
+  <!-- ----------------------------------
+			BIS1523/BIS2523 Documentation
+   Name:Wyatt Shanahan
+   Netid:wls219
+   Date:
+   
+   Variables used:
+      $var  <what var does>
+      
+  
+   -------------------------------------  -->
+</head>
+<body style="background-image:linear-gradient(to left,#186118,#3eb489,#186118)"> <!-- other colour: 00805b -->
+    <h1 id="header">Population Growth Projection Results</h1> <!--header-->
+</body>
+</html>
