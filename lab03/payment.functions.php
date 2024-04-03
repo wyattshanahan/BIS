b8512b2e9aa83de373b599795806914d1eb47454
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-04-03 09:39:02 -0500

    Add files via upload

---
+++ lab03/payment.functions.php
@@ -0,0 +1,79 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <title> Lecture 7 - Payment Results </title>
+    <link rel="stylesheet" href="payment.css">
+  <!-- ----------------------------------
+			BIS1523/BIS2523 Documentation
+   Name:  Steve Canfield
+   Netid: sec9
+   Date:  recently
+   
+   Variables used:
+        $balance    input       starting cc balance
+        $credits    input       how much we paid off
+        $charges    input       how much we spent
+        
+        $new_balance calculated this months balance
+        $min_payment calculated minimum payment
+        
+        $data_is_okay       flag variable for data validation
+        
+   -------------------------------------  -->  
+</head>
+<body>
+
+  <header> 
+    <h1> Payment Results </h1>
+  </header>
+
+  <section id="description">
+    <h2> Details </h2>
+    <p> The results of your calculate are displayed
+        to the right.  Minimum payment is 2% of your 
+        new balance.  Your card has a 15% annual rate interest rate.
+       </p>
+    <p> To perform a new calculation, <a href="payment_input.html"> click here. </a> </p>
+  </section>
+
+  <section id="io_area">
+   
+<?	
+    // input
+        $balance = $_POST['balance'];
+        $charges = $_POST['charges'];
+        $credits = $_POST['credits'];   
+        
+        $data_is_okay = true;
+        if (!is_numeric($balance)) {
+            print "Balance should be numeric <br />"; 
+            $data_is_okay = false; 
+        }
+        if (!is_numeric($charges)) {
+            print "Charges should be numeric <br />"; 
+            $data_is_okay = false; 
+        }
+        if (!is_numeric($credits)) {
+            print "Credits should be numeric <br />"; 
+            $data_is_okay = false; 
+        }        
+        
+        if ($data_is_okay) {
+            // process
+                $new_balance = $balance + $charges - $credits  + ($balance * 0.15 / 12);  // interest calculated in ()'s
+                if ($new_balance > 20) {
+                    $min_payment = $new_balance * 0.02;       
+                } else {
+                    $min_payment = $new_balance; 
+                }
+        
+            // output
+            print "Your new balance is $" . number_format($new_balance,2) . " <br />
+                   Your minimum payment this month is: $".number_format($min_payment,2);
+        }
+?>
+
+  </section>
+
+</body>
+</html>
