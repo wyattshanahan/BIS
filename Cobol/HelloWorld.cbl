a0c955b834c8f983753f25a993b817b9c53364ba
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-08-21 18:14:12 -0500

    add assignment 1

---
+++ Cobol/HelloWorld.cbl
@@ -0,0 +1,11 @@
+000100 identification division.
+000200 program-id. HelloWorld.
+000300 
+000400 environment division.
+000600 
+000700 data division.
+000800 
+000900 procedure division.
+001000 000-main.
+001100	display "Hello, World!".
+001200	stop run.
