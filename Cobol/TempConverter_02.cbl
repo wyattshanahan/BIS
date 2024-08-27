33f5c0bc5a22e12fcf9c27bf1bc99e07139e1877
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-08-27 08:34:58 -0500

    add initial version

---
+++ Cobol/TempConverter_02.cbl
@@ -0,0 +1,14 @@
+       identification division.
+       program-id. TempConverter.
+
+       environment division.
+
+       data division.
+
+       procedure division.
+       000-main.
+           display ' '.
+           display "Welcome to temperature converter".
+           display ' '.
+           display 'Enter a fahrenheit temperature to convert to celsius...'.
+           stop run.
