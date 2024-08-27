4201e124ba7b0ea00db4aeeb6e05d22a5ab9d8ae
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-08-27 18:48:22 -0500

    Add files via upload

--- Cobol/assignment02/TempConverter_02.cbl
+++ Cobol/assignment02/TempConverter_02.cbl
@@ -4,11 +4,21 @@
        environment division.
 
        data division.
-
+       
+       working-storage section.
+       01 nFah pic s999999v99.
+       01 neCent pic s999999v99.
+       
        procedure division.
        000-main.
-           display ' '.
+           display " ".
            display "Welcome to temperature converter".
-           display ' '.
-           display 'Enter a fahrenheit temperature to convert to celsius...'.
+           display " ".
+           display "Enter a fahrenheit temp to convert to celsius: ".
+           accept nFah.
+           compute neCent = (nFah - 32) * 0.5556.
+           display "The value in celsius is " neCent.
+           display " ".
+           
            stop run.
+           
