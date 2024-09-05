e00d811a0bc3744d82d2014d8cebe10884b59b0f
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-09-05 07:52:49 -0500

    Create hw05.php

---
+++ WebDev2/hw05/hw05.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>  </title>
+  <link rel="stylesheet" href="style.css"> 
+  <script src="hw05.js"> </script>
+</head>
+<body>
+    <h1>Temperature Conversion Utility</h1>
+    <div id="input">
+        <form name="inputForm">
+            Temperature: <input type="text" name="tempIn" size="5" /> <br />
+            <br />
+            <input type="radio" name="units" value="F"> Convert to Fahrenheit <br />
+            <input type="radio" name="units" value="C"> Convert to Celcius <br /> <br />
+            <div id="button_area">
+                <input type="submit" id="calc" value="Submit">
+            </div>
+        </form>
+    </div>
+
+    <div id="results">
+    
+    
+    
+    </div>
+</body>
+</html>
