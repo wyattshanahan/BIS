42cc1e619d1e118a08e5627c89586628e0ce3291
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-09-17 22:58:03 -0500

    Add files via upload

--- Cobol/code/assignment03/States-01.cbl
+++ Cobol/code/assignment03/States-01.cbl
@@ -5,19 +5,23 @@
        environment division.
        input-output section.
        file-control.
-          select infile assign to "states.txt" 
+          select infile assign to "States.txt" 
               organization is line sequential.
-	   
+	   select outfile assign to "States-01-output.txt"
+              organization is line sequential.
        data division.
        file section. 
        fd infile.
        01 xInput.
           05 nstateNum pic 99.
-          05 xInState pic xx.
+          05 xStateAbbrev pic xx.
           05 xregCode pic x.
-          05 xstate pic x(20).
+          05 xInState pic x(20).
           05 npop pic 9(8).
           05 nlandArea pic 9(6).
+       
+       fd outfile.
+       01 xFileOut pic x(80).
        working-storage section.
        77 xEoF       pic x         value 'n'.
        77 nCount     pic 999       value 0.
@@ -50,15 +54,13 @@
        
        01 xOutputFooter.
           05 filler       pic x(21) value "51 states            ".
-          05 neTotalPop   pic zzz,zzz,zz9 value 0.
+          05 neTotalPop   pic zzz,zzz,zz9.
           05 filler       pic x(3) value "   ".
-          05 neTotalArea  pic z,zzz,zz9 value 0.
+          05 neTotalArea  pic z,zzz,zz9.
           05 filler       pic x(5) value "     ".
           05 neTotalDen   pic z9.99.
-      * spacing guide: SN - 2 p - 5 a - 2
        procedure division.
        000-main.
-           display "Hello".
            perform 100-initialise.
            perform 200-loop until xEoF = 'y'.
            perform 300-terminate.
@@ -66,8 +68,13 @@
           
        100-initialise.
            open input infile.
+           open output outfile.
            display xOutputHeader.
            display xOutputLines.
+           move xOutputHeader to xFileOut.
+           write xFileOut.
+           move xOutputLines to xFileOut.
+           write xFileOut.
            
        200-loop.
            read infile
@@ -85,6 +92,8 @@
            compute nTotalPop = nTotalPop + npop.
            compute nTotalArea = nTotalArea + nlandArea.
            display xOutput.
+           move xOutput to xFileOut.
+           write xFileOut.
            
        300-terminate.
            close infile.
@@ -94,3 +103,8 @@
            move nTotalPop to neTotalPop.
            display xOutputLines.
            display xOutputFooter.
+           move xOutputLines to xFileOut.
+           write xFileOut.
+           move xOutputFooter to xFileOut.
+           write xFileOut.
+           close outfile.
