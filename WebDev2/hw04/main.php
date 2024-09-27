02dd59b582d582a8ff20a9e2d10202a8d6ddd066
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-09-27 03:15:04 -0500

    Update main.php

--- WebDev2/hw04/main.php
+++ WebDev2/hw04/main.php
@@ -10,98 +10,77 @@
 			BIS2523 Documentation
    Name:
    Netid:
-   Date:
+   Date: 26 September 2024
    
    Variables used:
-      $names  <store student names file content>
-      $grades <store grades>
-      $i <iterator>
-      $sel <user selected grade>
+      $dbc  <store DB connexion>
+      $query <store SQL query prior to execution>
+      $courses <store mysql object from courses>
+      $row < iterator for sql objects>
+      $button <used to form string for radio buttons>
+      $buttonval <used to store course ids>
+      $sel <store selection from button>
+      $tcResult < store result from taken_course>
+      $grades <array to store grades>
+      $studentNums <array to store student ID nums>
+      $studentList <sql object of student db>
+      $studentnames <array storing student names matched from taken_student>
   
    -------------------------------------  -->
 </head>
 <body>
+        <h1 id="header">Homework 4</h1>
     <form id="input" method="post" action = "main.php">
         <?
         $dbc = new mysqli("localhost", "student", "password", "university"); // db connexion
         $query = "select * from courses";
-        $courses = $dbc->query($query);
+        $courses = $dbc->query($query); // can use printr to iterate over an array
 
     while ($row = $courses->fetch_assoc()){ // iterate over the funny values
-        $buttonInt = $row['dept'].$row['code']; // build a string for each button
         $button = $row['dept'].' '.$row['code']; // build external button
-        print "<input type='radio' name = 'course' value = $buttonInt>$button<br />"; // output buttons
+        $buttonval = $row['course_id'];
+        print "<input type='radio' name = 'course' value = $buttonval>$button<br />"; // output buttons
     }
     print "<br />
     <input type='submit' name='Submit' value='Submit'>";
         ?>
     </form>
-    <!-- this part below is incomplete and needs finishing
     <div id="output">
         <?
         $sel = $_POST['course'];
         if ($sel != ""){
-            $query = "select * from courses_taken";
-            $result = $dbc->query($query);
-            print $result;
-            print $query;
-        print $sel;}
+            $query = "SELECT * FROM courses_taken WHERE taken_course = $sel";
+            $tcResult = $dbc->query($query);
+            if (!$tcResult){ // troubleshooting, throw an error if query fails
+                die("Query failed: " . $dbc->error);
+            }
+            $grades = array(); // create array to store grades
+            $studentNums = array();
+            while ($row = $tcResult->fetch_assoc()){ // iterate over the funny values
+              $grades[]=$row['grade']; // store grades in $grades array
+              $studentNums[]=$row['taken_student']; // add studentIDs to search for
+            }
+            $query = "SELECT * FROM student";
+            $studentList = $dbc->query($query);
+            $studentNames = array(); // create array to store names
+            while ($row = $studentList->fetch_assoc()){
+                if (in_array($row['student_id'], $studentNums)){
+                    $studentNames[]=$row['fname'].' '.$row['lname'];
+                }
+            }
+            print "<table> <tr>
+            <th>Name</th>
+            <th>Grade</th>
+            </tr>";
+            for ($i = 0; $i < count($studentNames); $i++){
+                print"<tr>
+                <td>$studentNames[$i]</td>
+                <td>$grades[$i]</td>
+                </tr>";
+            }
+            print"</table>";
+        }
         ?>
     </div>
-    
-
-    
-    
-    
-    
-    <?/*
-        $i = 0; //initialise i
-        $names = file("student.names.txt", FILE_IGNORE_NEW_LINES); //open file
-        $grades = file("student.grades.txt", FILE_IGNORE_NEW_LINES); //open/file
-        $sel = $_POST['grade']; //post selection
-        if ($sel != ""){
-        print"<section id='gradeTable'>
-        <table>";
-        print" <tr>
-                <th>Name</th>
-                <th>Grade</th>
-            </tr>";
-        }
-        foreach($grades as $i=>$grades[$i]){
-            if ($sel == 'A' and $grades[$i] > 90){
-                print"<tr>";
-                print"<td> $names[$i] </td>";
-                print "<td> $grades[$i] </td>";
-                print "</tr>";
-            }
-            elseif ($sel == 'B' and $grades[$i] >= 80 and $grades[$i] < 90){
-                print"<tr>";
-                print"<td> $names[$i] </td>";
-                print "<td> $grades[$i] </td>";
-                print "</tr>";
-            }
-            elseif ($sel == 'C' and $grades[$i] >= 70 and $grades[$i] < 80){
-                print"<tr>";
-                print"<td> $names[$i] </td>";
-                print "<td> $grades[$i] </td>";
-                print "</tr>";
-            }
-            elseif ($sel == 'D' and $grades[$i] >= 60 and $grades[$i] < 70){
-                print"<tr>";
-                print"<td> $names[$i] </td>";
-                print "<td> $grades[$i] </td>";
-                print "</tr>";
-            }
-            elseif ($sel == 'F' and $grades[$i] <60){
-                print"<tr>";
-                print"<td> $names[$i] </td>";
-                print "<td> $grades[$i] </td>";
-                print "</tr>";
-            }
-        }
-        print"</table>
-    </section>";
-    */
-    ?>
 </body>
 </html>
