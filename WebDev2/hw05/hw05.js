52cdae7c91955a7e65c2a8a0dc4efcfef9c5f150
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-10-03 08:50:47 -0500

    Update hw05.js

--- WebDev2/hw05/hw05.js
+++ WebDev2/hw05/hw05.js
@@ -1,15 +1,24 @@
-window.addEventListener("load", link_events, false);
+window.addEventListener("load", link_events);
 
 function link_events(){
-    document.getElementById("calc").onclick = Calculate;
+    document.forms["inputForm"]["submitButton"].onclick = Calculate();
 }
 
 function Calculate(){
     // input
     //var temp = "ello";
-    //var unit = document.forms["inputForm"]["units"].value; // the moment i try to set var from doc.forms, it force reloads
-    //var temp = document.forms["inputForm"]["temp"].value;
-    //alert(temp);
+    var units = document.forms["inputForm"]["units"].value; // the moment i try to set var from doc.forms, it force reloads
+    var temp = document.forms["inputForm"]["temp"].value;
+    
+    // data validation
+    var dataValid = true;
+    if (isNaN(temp)){
+        alert("Temperature should be numeric");
+        dataValid = false;
+    }
+    //alert("bonjour");
     document.getElementById("results").innerHTML = "Hello";
+    
     return false;
 }
+
