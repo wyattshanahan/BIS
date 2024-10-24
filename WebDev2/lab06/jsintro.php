30ffee4444afb2693350103801fbc95a1cf7164a
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-10-24 14:18:35 -0500

    Create jsintro.php

---
+++ WebDev2/lab06/jsintro.php
@@ -0,0 +1,21 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title> </title>
+  <link rel="stylesheet" href="jsintro.css">
+    <script src="jsintro.js" defer></script>
+</head>
+<body>
+
+<div id="mainarea">
+	<p id="aline"> This is a paragraph with an id. </p>
+
+	<p> This is a <span id="oneword">1</span> with an id. </p>
+
+</div>
+<br><hr><br>
+<div id="outarea">
+This is a div with an id.  
+</div>
+</body>
+</html>
