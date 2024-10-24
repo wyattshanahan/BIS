647a3e055694d87c137a267126e9676778a20ceb
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-10-23 22:49:06 -0500

    Create hw06.php

---
+++ WebDev2/hw06/hw06.php
@@ -0,0 +1,26 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>  </title>
+  <link rel="stylesheet" href="style.css"> 
+  <script src="hw06.js"> </script>
+</head>
+<body>
+    <div id="input">
+        <h2>hw06-Population Growth</h2>
+        <form name="inputForm">
+            Years to Forecast: <input type="text" name="years" size="8" /></br>
+            Current Population: <input type="text" name="pop" size="12" /></br>
+            Growth Rate: <input type="text" name = "rate" size = "12" /></br>
+            <br />
+            <div id="button_area">
+                <input type="submit" id="submitButton" value="Submit">
+            </div>
+        </form>
+    </div>
+
+    <div id="results">
+    
+    </div>
+</body>
+</html>
