07ef909bbb6768d8d9922bc3f2f60fd9344ed545
Wyatt Shanahan <116302220+wyattshanahan@users.noreply.github.com>
2024-10-29 09:00:34 -0500

    Update hw06.php

--- WebDev2/hw06/hw06.php
+++ WebDev2/hw06/hw06.php
@@ -9,9 +9,9 @@
     <div id="input">
         <h2>hw06-Population Growth</h2>
         <form name="inputForm">
-            Years to Forecast: <input type="text" name="years" size="8" /></br>
-            Current Population: <input type="text" name="pop" size="12" /></br>
-            Growth Rate: <input type="text" name = "rate" size = "12" /></br>
+            Years to Forecast: <input type="text" name="years" size="8" required/></br>
+            Current Population: <input type="text" name="pop" size="12" required/></br>
+            Growth Rate: <input type="text" name = "rate" size = "12" required/></br>
             <br />
             <div id="button_area">
                 <input type="submit" id="submitButton" value="Calculate!">
